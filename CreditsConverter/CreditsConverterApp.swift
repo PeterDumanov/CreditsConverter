df9b6a21037ee1b50561e5cac8c23e786a8c8f9c
PeterDumanov <dumanovkrd@yandex.ru>
2025-05-07 23:10:21 +0200

    Initial Commit

---
+++ CreditsConverter/CreditsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditsConverterApp.swift
+//  CreditsConverter
+//
+//  Created by Petr Dumanov on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CreditsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
