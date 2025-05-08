d768884c321c69cb34064fb16aaa7a0279150694
PeterDumanov <dumanovkrd@yandex.ru>
2025-05-08 19:55:56 +0200

    Add currency texts

--- CreditsConverter/ContentView.swift
+++ CreditsConverter/ContentView.swift
@@ -11,28 +11,33 @@ struct ContentView: View {
     
     @State private var isFirstSelected = true
     @State private var isSecondSelected = false
+    @State var currencies: [(name: String, value: String, isSelected: Bool)] = [
+        ("Credit", "", true),
+        ("Dollar", "", false),
+        ("Euro", "", false)
+    ]
 
+    
     var body: some View {
-        Text("PressMe")
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(isFirstSelected ? Color.orange : Color.blue, lineWidth: 2)
-                    )
-                    .onTapGesture {
-                        isFirstSelected.toggle()
-                        isSecondSelected.toggle()
+        
+        ForEach(currencies, id: \.name) {currency in
+            Text(currency.name)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(currency.isSelected ? Color.orange : Color.blue, lineWidth: 2)
+                )
+                .onTapGesture {
+                    for i in 0..<currencies.count {
+                        if currencies[i].name == currency.name {
+                            currencies[i].isSelected.toggle()
+                        } else {
+                            currencies[i].isSelected = false
+                        }
                     }
-        Text("PressMe")
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(isSecondSelected ? Color.orange : Color.blue, lineWidth: 2)
-                    )
-                    .onTapGesture {
-                        isSecondSelected.toggle()
-                        isFirstSelected.toggle()
-                    }
+                }
+        }
     }
 }
 
