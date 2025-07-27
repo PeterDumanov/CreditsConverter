f106f7ae7d256d8695ae5529d5a0f5bfe2aefe81
PeterDumanov <dumanovkrd@yandex.ru>
2025-07-27 21:34:06 +0200

    Add project

--- CreditsConverter/ContentView.swift
+++ CreditsConverter/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    var currencies = ["EUR", "USD", "CRD"]
+    @State var values = Array(repeating: "0", count: 3)
+    @State var selectedCurrencyIndex = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            ForEach(currencies.indices, id: \.self) { index in
+                Text(values[index])
+                    .frame(maxWidth: .infinity)
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 2)
+                    )
+                    .simultaneousGesture(TapGesture().onEnded {
+                        selectedCurrencyIndex = index
+                        print(selectedCurrencyIndex)
+                    })
+            }
         }
         .padding()
     }
