9a33e5ab17f965fc2d1cb788d9230267f4c1661c
PeterDumanov <dumanovkrd@yandex.ru>
2025-07-31 19:26:16 +0200

    Implement button functionality

--- CreditsConverter/ContentView.swift
+++ CreditsConverter/ContentView.swift
@@ -12,6 +12,13 @@ struct ContentView: View {
     @State var values = Array(repeating: "0", count: 3)
     @State var selectedCurrencyIndex = 0
     
+    let buttons = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+        ["C", "0", "←"]
+    ]
+    
     var body: some View {
         VStack(spacing: 16) {
             ForEach(currencies.indices, id: \.self) { index in
@@ -20,15 +27,54 @@ struct ContentView: View {
                     .padding(10)
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blue, lineWidth: 2)
+                            .stroke(index == selectedCurrencyIndex ? Color.orange : Color.blue, lineWidth: 2)
                     )
                     .simultaneousGesture(TapGesture().onEnded {
                         selectedCurrencyIndex = index
                         print(selectedCurrencyIndex)
                     })
             }
+            
+            ForEach(buttons, id: \.self) { buttonsRow in
+                HStack {
+                    ForEach(buttonsRow, id: \.self) { buttonText in
+                        Button {
+                            buttonPressed(buttonText)
+                        } label: {
+                            Text(buttonText)
+                                .frame(width: 80, height: 80)
+                                .overlay(
+                                    Circle()
+                                        .stroke(Color.blue, lineWidth: 2)
+                                )
+                        }
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    private func buttonPressed(_ buttonText: String) {
+        switch buttonText {
+        case "C":
+            values[selectedCurrencyIndex] = "0"
+        case "←":
+            if values[selectedCurrencyIndex].count > 1 {
+                values[selectedCurrencyIndex].removeLast()
+                if values[selectedCurrencyIndex].last == "." {
+                    values[selectedCurrencyIndex].removeLast()
+                }
+            } else {
+                values[selectedCurrencyIndex] = "0"
+            }
+        default:
+            if values[selectedCurrencyIndex] == "0" {
+                values[selectedCurrencyIndex] = buttonText
+            } else {
+                values[selectedCurrencyIndex] += buttonText
+            }
+        }
     }
 }
 
